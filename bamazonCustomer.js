f547c0a423bb4c8f022ac568ce13f73b35ea43d4
Nerita Sung <neritasung@gmail.com>
2018-06-03 22:18:02 -0500

    adds functions and psuedocode

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -30,7 +30,40 @@ function displayProducts() {
     // query the database for all items 
     connection.query("SELECT ITEM_ID, PRODUCT_NAME, PRICE FROM auctions", function (err, results) {
         if (err) throw err;
-
     });
-    
+
+    // run Start function
+    start();
 };
+
+
+// -------- Start Function ---------//
+function start() {
+    inquirer
+    .prompt([
+      {
+        name: "item_id",
+        type: "input",
+        message: "What product would you like to buy? Enter the item id here."
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many do you want?"
+      },
+    ])
+        .then(function (answer) {
+        
+            // compare the item_id user entered and quantity against DB
+
+
+            // if quantity is <= stock quantity, calculate the total price - Quantity user entered * price
+
+
+            // else statement quantity user entered > stock display Insufficient quantity! message
+        });
+
+
+            // call displayProcut again
+
+}
